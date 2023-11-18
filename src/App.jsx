0878674d05cd3f08594a39e3251b37b70792d17e
bilal65600 <bilalahmad65600@gmail.com>
2023-11-18 05:45:39 +0500

    inti basic app

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import "./style.css";
+
+
+function Cv(){
+  
+
+  return (
+<div className="main">
+    <div className="left">
+      <h2>About</h2>
+    <p>I am a keen, hard working,</p><p> reliable and excellent time keeper</p>
+    <h4>032300000000</h4>
+    <h4>Lahore Pakistan</h4>
+   
+    </div>
+    <div className="right">
+      
+      <h2>Bilal Ahmad</h2>
+      <h3>Personal</h3>
+      <ul>
+        <li>Father Name: Muhammad Yaqoob</li>
+        <li>Contact: 0000000000</li>
+        <li>Email: abc@gmail.com</li>
+        
+      </ul>
+      <h3>Qualification</h3>
+      <ul>
+        <li>Master 2019</li>
+        <li>Inter 2012</li>
+        <li>Metric</li>
+        
+      </ul>
+      <h3>Hobbies</h3>
+      <ul>
+        <li>Cricket</li>
+        <li>football</li>
+        <li>tennis</li>
+        
+      </ul>
+      
+    </div>
+    </div>
+  )
+}
+export default Cv;
